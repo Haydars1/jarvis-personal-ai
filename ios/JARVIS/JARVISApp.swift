ef85e76e11c4d6ae00d54f0f1342298825d6af7b
Haydars1 <164067724+Haydars1@users.noreply.github.com>
2026-09-12 17:28:16 +0200

    Wire native notification delegate

--- ios/JARVIS/JARVISApp.swift
+++ ios/JARVIS/JARVISApp.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 @main
 struct JARVISApp: App {
+    @UIApplicationDelegateAdaptor(NotificationManager.self) private var notificationManager
     @StateObject private var appState = AppState()
 
     var body: some Scene {
