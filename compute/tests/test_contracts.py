ee400f1f739c0bb7c32cd499f60f9c37a9e6b5aa
Haydars1 <164067724+Haydars1@users.noreply.github.com>
2026-09-17 20:15:25 +0200

    test(ecu): cover compute worker contracts

---
+++ compute/tests/test_contracts.py
@@ -0,0 +1,23 @@
+from ecu_worker.contracts import AnalysisJobInput, AnalysisJobOutput, build_run_fingerprint
+
+
+def test_run_fingerprint_is_deterministic_and_sensitive():
+    a = build_run_fingerprint('abc', 'model-1', 'rules-1', {'mode': 'analyze', 'x': 1})
+    b = build_run_fingerprint('abc', 'model-1', 'rules-1', {'x': 1, 'mode': 'analyze'})
+    c = build_run_fingerprint('abc', 'model-2', 'rules-1', {'mode': 'analyze', 'x': 1})
+    assert a == b
+    assert a != c
+    assert len(a) == 64
+
+
+def test_analysis_job_input_defaults_to_safe_analysis():
+    job = AnalysisJobInput(job_id='j1', artifact_sha256='a' * 64, artifact_uri='r2://bucket/originals/x')
+    assert job.operation == 'analyze'
+    assert job.paid_api_allowed is False
+
+
+def test_output_has_explicit_unknown_support_state():
+    output = AnalysisJobOutput(job_id='j1', run_fingerprint='b' * 64)
+    assert output.status == 'NEEDS_REVIEW'
+    assert output.ecu_family == 'UNKNOWN'
+    assert output.supported is False
